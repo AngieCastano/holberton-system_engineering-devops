33983f97781d483846c0f6a3c2db088ffdcf3e16
AngieCastano1634 <1634@holbertonschool.com>
2020-08-03 18:36:09 -0500

    killing a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 #killing a process
 exec{'killing_process':
-  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => pkill killmenow
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => pkill killmenow,
+  provider => 'shell'
 }
