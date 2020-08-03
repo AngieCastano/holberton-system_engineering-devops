1a160b480e5fbc0b6a9710bde98c95d1ba5597d0
AngieCastano1634 <1634@holbertonschool.com>
2020-08-03 16:33:09 -0500

    Puppet, create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 # Creating a file with puppet inside the tmp directory
 
 file { '/tmp/holberton':
-  ensure  => file,
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
