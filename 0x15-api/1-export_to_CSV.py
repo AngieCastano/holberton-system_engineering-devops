08b1f0c3aed22a169e28b9407c91e0d78111f41b
AngieCastano1634 <1634@holbertonschool.com>
2020-09-16 14:30:45 -0500

    [UPDATE][As it was before]

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,28 +6,24 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        url = "https://jsonplaceholder.typicode.com/"
-        getting = "users?id={}".format(sys.argv[1])
-        url = "https://jsonplaceholder.typicode.com/{}".format(getting)
-        response = requests.get(url)
-        list_of_dictionary = response.json()
-        us = name = list_of_dictionary[0].get("name")
-        user_name = list_of_dictionary[0].get("username")
-        getting = "todos?userId={}&completed=true".format(sys.argv[1])
-        url = "https://jsonplaceholder.typicode.com/{}".format(getting)
-        response = requests.get(url)
-        dt = list_of_done_tasks = response.json()
-        getting = "todos?userId={}".format(sys.argv[1])
-        url = "https://jsonplaceholder.typicode.com/{}".format(getting)
-        response = requests.get(url)
-        t = list_of_tasks = response.json()
+    url = "https://jsonplaceholder.typicode.com/"
+    getting = "users?id={}".format(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/{}".format(getting)
+    response = requests.get(url)
+    list_of_dictionary = response.json()
+    us = name = list_of_dictionary[0].get("name")
+    user_name = list_of_dictionary[0].get("username")
+    getting = "todos?userId={}&completed=true".format(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/{}".format(getting)
+    response = requests.get(url)
+    dt = list_of_done_tasks = response.json()
+    getting = "todos?userId={}".format(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/{}".format(getting)
+    response = requests.get(url)
+    t = list_of_tasks = response.json()
 
-        with open("USER_ID.csv", "w", newline="") as csv_file:
-            thewriter = csv.writer(csv_file, quotechar='"'\
-                                   , quoting=csv.QUOTE_ALL)
-            for task in list_of_tasks:
-                thewriter.writerow(["{}".format(sys.argv[1]), user_name,
-                                    task.get("completed"), task.get("title")])
-    except:
-        exit()
+    with open("USER_ID.csv", "w", newline="") as csv_file:
+        thewriter = csv.writer(csv_file, quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in list_of_tasks:
+            thewriter.writerow(["{}".format(sys.argv[1]), user_name,
+                                task.get("completed"), task.get("title")])
