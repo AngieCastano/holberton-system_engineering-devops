4dddd151010a9680c29e843e9d83b6d00cc07760
AngieCastano1634 <1634@holbertonschool.com>
2020-09-17 22:36:16 -0500

    [UPDATE][allow_redirections = False]

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,7 +10,10 @@ def recurse(subreddit, hot_list=[], after="start"):
         return(hot_list)
     url = "https://www.reddit.com/r/{}/.json?after={}".format(subreddit, after)
     headers = {'User-agent': 'Chrome'}
-    response = requests.get(url, headers=headers)
+    kwargs = {'allow_redirects': False, 'headers': headers, 'url': url}
+    response = requests.get(**kwargs)
+    if response.status_code != 200:
+        return(None)
     after = response.json()['data']['after']
     t = [item['data']['title'] for item in response.json()['data']['children']]
     hot_list.extend(t)
