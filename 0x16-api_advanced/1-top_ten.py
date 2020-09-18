20c8efbd3f88b6fc0332dcd02a8ee5a54be1915a
AngieCastano1634 <1634@holbertonschool.com>
2020-09-17 19:43:19 -0500

    [FIX][checks status_code / change logic]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,22 +9,20 @@ def top_ten(subreddit):
     titles = None
     url = "https://www.reddit.com/r/{}/.json?count=10".format(subreddit)
     headers = {'User-agent': 'Chrome'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print(titles)
-        return
-    list_of_dictionary = response.json()
-    for key, diction in list_of_dictionary.items():
-        if type(diction) == dict:
-            for mini_key, mini_value in diction.items():
-                if type(mini_value) == list:
-                    for mini_item in mini_value:
-                        if type(mini_item) == dict:
-                            for sub_key, sub_value in mini_item.items():
-                                if type(sub_value) == dict:
-                                    for m_s_k, m_s_value in sub_value.items():
-                                        if m_s_k == "title":
-                                            titles = m_s_value
-                                            print(titles)
-    if not titles:
-        print(titles)
+        print(None)
+    else:
+        list_of_dictionary = response.json()
+        for key, diction in list_of_dictionary.items():
+            if type(diction) == dict:
+                for mini_key, mini_value in diction.items():
+                    if type(mini_value) == list:
+                        for mini_item in mini_value:
+                            if type(mini_item) == dict:
+                                for sub_key, sub_value in mini_item.items():
+                                    if type(sub_value) == dict:
+                                        for m_s_k, m_s_v in sub_value.items():
+                                            if m_s_k == "title":
+                                                titles = m_s_v
+                                                print(titles)
