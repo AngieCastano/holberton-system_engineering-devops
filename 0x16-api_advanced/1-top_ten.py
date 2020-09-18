1124e8b5d3e1325b185048196aa1fa47d6813431
AngieCastano1634 <1634@holbertonschool.com>
2020-09-17 19:51:43 -0500

    [FIX][limit=10]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,22 +7,21 @@ import requests
 def top_ten(subreddit):
     """prints the titles of the first10hotpostslisted for a given subreddit"""
     titles = None
-    url = "https://www.reddit.com/r/{}/.json?count=10".format(subreddit)
+    url = 'https://www.reddit.com/r/{}/.json?limit=10'.format(subreddit)
     headers = {'User-agent': 'Chrome'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-    else:
-        list_of_dictionary = response.json()
-        for key, diction in list_of_dictionary.items():
-            if type(diction) == dict:
-                for mini_key, mini_value in diction.items():
-                    if type(mini_value) == list:
-                        for mini_item in mini_value:
-                            if type(mini_item) == dict:
-                                for sub_key, sub_value in mini_item.items():
-                                    if type(sub_value) == dict:
-                                        for m_s_k, m_s_v in sub_value.items():
-                                            if m_s_k == "title":
-                                                titles = m_s_v
-                                                print(titles)
+    response = requests.get(url, headers=headers)
+    list_of_dictionary = response.json()
+    for key, diction in list_of_dictionary.items():
+        if type(diction) == dict:
+            for mini_key, mini_value in diction.items():
+                if type(mini_value) == list:
+                    for mini_item in mini_value:
+                        if type(mini_item) == dict:
+                            for sub_key, sub_value in mini_item.items():
+                                if type(sub_value) == dict:
+                                    for m_s_k, m_s_value in sub_value.items():
+                                        if m_s_k == "title":
+                                            titles = m_s_value
+                                            print(titles)
+    if not titles:
+        print(titles)
