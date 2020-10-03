d73d4cc008f375e7e23bacaccb6478cb30b462c4
AngieCastano1634 <1634@holbertonschool.com>
2020-10-02 20:57:59 -0500

    [ADD][json of every id]

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" This is about consuming APIs, GET mehod"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url_id = 'https://jsonplaceholder.typicode.com/users/'
+    get_the_ids = requests.get(url_id).json()
+    only_ids = []
+    for i, mini_dict in enumerate(get_the_ids):
+            only_ids.append(get_the_ids[i].get("id"))
+    last_dictionary = {}
+    for _id in only_ids:
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(_id)
+        url_1 = f'https://jsonplaceholder.typicode.com/todos?userId={_id}'
+        all_todo = requests.get(url_1)
+        user_name = requests.get(url).json()['username']
+        inner_list = []
+
+        for task in all_todo.json():
+            inner_list.append(
+                {"task": task.get("title"),
+                 "completed": task.get("completed"),
+                 "username": user_name}
+            )
+        to_update = {_id: inner_list}
+        last_dictionary.update(to_update)
+
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(last_dictionary, f)
